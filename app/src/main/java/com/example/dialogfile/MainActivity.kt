c4b262b2d9eff0a58d665c0293d949e705d8ff67
kovalevdmv@yandex.ru <kovalevdmv@yandex.ru>
2019-09-22 01:09:27 +0300

    init

--- app/src/main/java/com/example/dialogfile/MainActivity.kt
+++ app/src/main/java/com/example/dialogfile/MainActivity.kt
@@ -64,7 +64,7 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         try {
             for (line in File(data?.data?.path?.split(":")?.get(1)).readLines()) {
-
+                textView.text = textView.text.toString() + line.toString()+"\n"
             }
         } catch (e : Exception){
             textView.text = textView.text.toString() + e.toString()
@@ -73,30 +73,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupPermissions() {
-        // Here, thisActivity is the current activity
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_EXTERNAL_STORAGE)
             != PackageManager.PERMISSION_GRANTED) {
-
-            // Permission is not granted
-            // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
             } else {
-                // No explanation needed, we can request the permission.
                 ActivityCompat.requestPermissions(this,
                     arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
                     1)
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
         } else {
-            // Permission has already been granted
         }
     }
 
@@ -109,11 +96,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun click(view: View){
-
         setupPermissions()
-
         showFileChooser()
-
-
     }
 }
